22a1dbc2d37acfec0e7daee668ff8a5fd817f760
Martin <61944410+martinjoshevski@users.noreply.github.com>
2021-06-20 21:20:33 +0200

    Favicon update

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home({ products }) {
   return (
     <div className="bg-gray-100">
       <Head>
-        <title>Amazon.clone</title>
+        <title>Amazon</title>
       </Head>
       <Header />
       <main className="max-w-screen-2xl mx-auto">
