28f8f3c8534a5bceea243e6888c25473c4d3abdd
Martin <61944410+martinjoshevski@users.noreply.github.com>
2021-07-07 17:20:11 +0200

    Final Build

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,6 +1,8 @@
 import { StarIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket, removeFromBasket } from "../slices/basketSlice";
 
 function CheckoutProduct({
   id,
@@ -11,6 +13,22 @@ function CheckoutProduct({
   category,
   image,
 }) {
+  const dispatch = useDispatch();
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      rating,
+      description,
+      category,
+      image,
+    };
+    dispatch(addToBasket(product));
+  };
+  const removeItemFromBasket = () => {
+    dispatch(removeFromBasket({ id }));
+  };
   return (
     <div className="grid grid-cols-5">
       <Image src={image} height={200} width={200} objectFit="contain" />
@@ -25,6 +43,14 @@ function CheckoutProduct({
         </div>
         <p className="text-xs my-2 line-clamp-3">{description}</p>
         <Currency quantity={price} />
+      </div>
+      <div className="flex flex-col space-y-2 my-auto justify-end">
+        <button className="button" onClick={addItemToBasket}>
+          Add To Basket
+        </button>
+        <button className="text-gray-500" onClick={removeItemFromBasket}>
+          Remove from Basket
+        </button>
       </div>
     </div>
   );
